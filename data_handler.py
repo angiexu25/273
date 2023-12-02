28fe3337ace8ce186c2ecd3b58a28b6dbb580cc4
angiexu25 <54309744+angiexu25@users.noreply.github.com>
2023-12-01 21:48:56 -0800

    Add files via upload

--- data_handler.py
+++ data_handler.py
@@ -2,6 +2,20 @@ import os
 from PIL import Image
 import pandas as pd
 from torch.utils.data import Dataset
+import torch
+
+'''
+anger = 0
+contempt = 1
+disgust = 2
+fear = 3
+happiness = 4
+neutral = 5
+sadness = 6
+surprise = 7
+'''
+
+label_mapping = {"anger": 0, "contempt": 1, "disgust": 2, "fear": 3, "happiness": 4, "neutral": 5, "sadness": 6, "surprise": 7} 
 
 class FacialExpressionsDataset(Dataset):
     def __init__(self, csv_file, img_dir, transform=None):
@@ -15,10 +29,12 @@ class FacialExpressionsDataset(Dataset):
     def __getitem__(self, index):
         img_name = self.annotations.iloc[index, 1]  # Adjust column index if necessary
         img_path = os.path.join(self.img_dir, img_name)
-        #if not os.path.exists(img_path):
-        #    return None  # Return None if the image does not exist
         image = Image.open(img_path).convert('L')  # Convert to grayscale
         label = self.annotations.iloc[index, 2]  # Adjust column index if necessary
+        label = label_mapping[label.lower()]  # Convert label string to integer
+        label = torch.tensor(label, dtype=torch.long)# Convert label to a tensor of type long
+        
         if self.transform:
             image = self.transform(image)
+
         return (image, label)
